dd88899bf96e23a2cd3eaf0771a5c313548b2274
dimpershko-a11y <dim.pershko@gmail.com>
2026-09-05 19:42:45 +0300

    v4.11.23: bump example version

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 window.PROCHISTKA_CONFIG = {
-  APP_VERSION: 'v4.11.22',
+  APP_VERSION: 'v4.11.23',
   APP_PASSWORD: '',
   // Повышайте число при изменении цен, чтобы они применились на всех устройствах.
   CONFIG_REVISION: 1,
